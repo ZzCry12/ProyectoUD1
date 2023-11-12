f9662c81fc30f9ea1e0185e04ed1e3b23b995db2
ZzCry12 <dariorata@gmail.com>
2023-11-12 18:47:13 +0100

    añadidos menores

--- app/src/main/java/com/example/proyectoud1/FirstFragment.java
+++ app/src/main/java/com/example/proyectoud1/FirstFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -38,6 +41,22 @@ public class FirstFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/com/example/proyectoud1/MainActivity.java
+++ app/src/main/java/com/example/proyectoud1/MainActivity.java
@@ -52,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
+        if (id == R.id.action_refresh) {
             return true;
         }
 
